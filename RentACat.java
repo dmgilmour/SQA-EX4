1d272f7c422700f1cbbaee6901e10c5495e2c1a4
dmgilmour <dmgilmour@pitt.edu>
2017-06-15 13:19:30 -0400

    WIP

--- RentACat.java
+++ RentACat.java
@@ -1,8 +1,64 @@
 import java.util.*;
 
 public class RentAClass {
+
+	Scanner input = new Scanner(System.in);
+
 	public static void main(String[] args) {
 		
 
 	}
+
+	public int promptDesiredCat(ArrayList<Cat> cats) {
+		int desiredCat = 0;
+		while (desiredCat == 0) {
+			System.out.print("Rent which cat? > ");
+			try {
+				desiredCat = Integer.parseInt(input.next());
+				for (givenCat : cats) {
+					if (desiredCat == givenCat.id) {
+						if (!givenCat.id.rented) {
+							return desiredCat;
+						} else {
+							System.out.println("Sorry, " + givenCat.name + " is not here!");
+						}
+					}
+				}
+				System.out.println("Invalid Cat ID");
+			} catch (NumberFormatException e) {
+				System.out.println("Invalid Cat ID");
+			}
+		}
+	}
+
+	public int promptDesiredCat(ArrayList<Cat> cats) {
+		int desiredCat = 0;
+		while (desiredCat == 0) {
+			System.out.print("Rent which cat? > ");
+			try {
+				desiredCat = Integer.parseInt(input.next());
+				for (givenCat : cats) {
+					if (desiredCat == givenCat.id) {
+						if (!givenCat.id.rented) {
+							return desiredCat;
+						} else {
+							System.out.println("Sorry, " + givenCat.name + " is not here!");
+						}
+					}
+				}
+				System.out.println("Invalid Cat ID");
+			} catch (NumberFormatException e) {
+				System.out.println("Invalid Cat ID");
+			}
+		}
+	}
+
+	public void printAvailableCats(ArrayList<Cat> cats) {
+		System.out.println("Cats for Rent");
+		for (givenCat : cats) {
+			System.out.println(givenCat.toString());
+		}
+	}
+
+		
 }
