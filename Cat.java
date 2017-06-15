5d862f03bb3e0e57f4c667544e298724c3dce041
dmgilmour <dmgilmour@pitt.edu>
2017-06-15 12:58:48 -0400

    Basic classes

---
+++ Cat.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class Cat {
+
+	private static int id;
+	private static float cost;
+	private static String name;
+	private static boolean rented;
+
+	public Cat(int id, float cost, String name) {
+	
+		this.id = id;
+		this.cost = cost;
+		this.name = name;
+		rented = false;
+
+	}	
+}
---
+++ Customer.java
@@ -0,0 +1,14 @@
+
+
+public class Customer {
+
+	private static String name;
+	private static float money;
+	private static Cat rentedCat;
+
+	public Customer(String name, float money) {
+		this.name = name;
+		this.money = money;
+		rentedCat = null;
+	}
+}
